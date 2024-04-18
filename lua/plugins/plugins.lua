f29083834f4e6d96733039930de8bbaddc44babe
Lucas <3806706+GrootBeard@users.noreply.github.com>
2024-04-18 16:48:50 +0200

    added lualine

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -54,15 +54,15 @@ return {
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
